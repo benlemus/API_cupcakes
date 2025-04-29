0a015ca12124620bb6d9e927212ce057486bcdff
benlemus <benlemus234@gmail.com>
2025-04-29 01:17:01 -0700

    created Cupcake model. created JSON returns for all_cupcakes, individual cupcake(by id), and add a cupcake(201 status). fixed tests.py and seed.py to include "with app.app_context".

--- app.py
+++ app.py
@@ -1,1 +1,41 @@
 """Flask app for Cupcakes"""
+from flask import Flask, jsonify, request
+from models import db, connect_db, Cupcake
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['SECRET_KEY'] = 'shhhh'
+
+connect_db(app)
+
+@app.route('/api/cupcakes')
+def all_cupcakes():
+    cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
+    return jsonify(cupcakes=cupcakes)
+
+@app.route('/api/cupcakes/<int:id>')
+def cupcake_details(id):
+    data = Cupcake.query.get_or_404(id)
+    cupcake = data.serialize()
+    
+    return jsonify(cupcake=cupcake)
+
+@app.route('/api/cupcakes', methods=['POST'])
+def add_cupcake():
+    flavor = request.json['flavor']
+    size = request.json['size']
+    rating = request.json['rating']
+    image = request.json['image']
+
+    new_cupcake = Cupcake(flavor=flavor, size=size, rating=rating, image=image)
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    return (jsonify(cupcake=new_cupcake.serialize()),201)
+
+
--- tests.py
+++ tests.py
@@ -10,10 +10,6 @@ app.config['SQLALCHEMY_ECHO'] = False
 # Make Flask errors be real errors, rather than HTML pages with error info
 app.config['TESTING'] = True
 
-db.drop_all()
-db.create_all()
-
-
 CUPCAKE_DATA = {
     "flavor": "TestFlavor",
     "size": "TestSize",
@@ -28,82 +24,89 @@ CUPCAKE_DATA_2 = {
     "image": "http://test.com/cupcake2.jpg"
 }
 
+with app.app_context():
+    db.drop_all()
+    db.create_all()
 
-class CupcakeViewsTestCase(TestCase):
-    """Tests for views of API."""
+    class CupcakeViewsTestCase(TestCase):
+        """Tests for views of API."""
 
-    def setUp(self):
-        """Make demo data."""
+        def setUp(self):
+            """Make demo data."""
+            with app.app_context():
+                Cupcake.query.delete()
+                cupcake = Cupcake(**CUPCAKE_DATA)
+                db.session.add(cupcake)
+                db.session.commit()
 
-        Cupcake.query.delete()
+                self.cupcake = cupcake
+                self.id = cupcake.id
+                self.flavor = cupcake.flavor
+                self.size = cupcake.size
+                self.rating = cupcake.rating
+                self.image = cupcake.image
 
-        cupcake = Cupcake(**CUPCAKE_DATA)
-        db.session.add(cupcake)
-        db.session.commit()
+        def tearDown(self):
+            """Clean up fouled transactions."""
+            with app.app_context():
+                db.session.rollback()
 
-        self.cupcake = cupcake
+        def test_list_cupcakes(self):
+            with app.test_client() as client:
+                resp = client.get("/api/cupcakes")
 
-    def tearDown(self):
-        """Clean up fouled transactions."""
+                self.assertEqual(resp.status_code, 200)
 
-        db.session.rollback()
+                data = resp.json
+                self.assertEqual(data, {
+                    "cupcakes": [
+                        {
+                            "id": self.cupcake.id,
+                            "flavor": "TestFlavor",
+                            "size": "TestSize",
+                            "rating": 5,
+                            "image": "http://test.com/cupcake.jpg"
+                        }
+                    ]
+                })
 
-    def test_list_cupcakes(self):
-        with app.test_client() as client:
-            resp = client.get("/api/cupcakes")
+        def test_get_cupcake(self):
+            with app.test_client() as client:
+                url = f"/api/cupcakes/{self.cupcake.id}"
+                resp = client.get(url)
 
-            self.assertEqual(resp.status_code, 200)
-
-            data = resp.json
-            self.assertEqual(data, {
-                "cupcakes": [
-                    {
+                self.assertEqual(resp.status_code, 200)
+                data = resp.json
+                self.assertEqual(data, {
+                    "cupcake": {
                         "id": self.cupcake.id,
                         "flavor": "TestFlavor",
                         "size": "TestSize",
                         "rating": 5,
                         "image": "http://test.com/cupcake.jpg"
                     }
-                ]
-            })
+                })
 
-    def test_get_cupcake(self):
-        with app.test_client() as client:
-            url = f"/api/cupcakes/{self.cupcake.id}"
-            resp = client.get(url)
+        def test_create_cupcake(self):
+            with app.test_client() as client:
+                url = "/api/cupcakes"
+                resp = client.post(url, json=CUPCAKE_DATA_2)
 
-            self.assertEqual(resp.status_code, 200)
-            data = resp.json
-            self.assertEqual(data, {
-                "cupcake": {
-                    "id": self.cupcake.id,
-                    "flavor": "TestFlavor",
-                    "size": "TestSize",
-                    "rating": 5,
-                    "image": "http://test.com/cupcake.jpg"
-                }
-            })
+                self.assertEqual(resp.status_code, 201)
 
-    def test_create_cupcake(self):
-        with app.test_client() as client:
-            url = "/api/cupcakes"
-            resp = client.post(url, json=CUPCAKE_DATA_2)
+                data = resp.json
 
-            self.assertEqual(resp.status_code, 201)
+                # don't know what ID we'll get, make sure it's an int & normalize
+                self.assertIsInstance(data['cupcake']['id'], int)
+                del data['cupcake']['id']
 
-            data = resp.json
+                self.assertEqual(data, {
+                    "cupcake": {
+                        "flavor": "TestFlavor2",
+                        "size": "TestSize2",
+                        "rating": 10,
+                        "image": "http://test.com/cupcake2.jpg"
+                    }
+                })
 
-            # don't know what ID we'll get, make sure it's an int & normalize
-            self.assertIsInstance(data['cupcake']['id'], int)
-            del data['cupcake']['id']
-
-            self.assertEqual(data, {
-                "cupcake": {
-                    "flavor": "TestFlavor2",
-                    "size": "TestSize2",
-                    "rating": 10,
-                    "image": "http://test.com/cupcake2.jpg"
-                }
-            })
-
-            self.assertEqual(Cupcake.query.count(), 2)
+                self.assertEqual(Cupcake.query.count(), 2)
